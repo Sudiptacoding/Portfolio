c53ddb95b79a0e8706723f1ffa62315c76b46b54
Sudiptacoding <you@example.comsudiptabiswas506@gmail.com>
2023-12-10 18:16:23 +0600

    Side skills updated

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 
 const Banner = () => {
+    const onButtonClick = () => {
+        const pdfUrl = "https://drive.google.com/file/d/1TopQCNCyS28qG28XUIn71mVsbrJWoDnr/view?usp=sharing";
+        const link = document.createElement("a");
+        link.href = pdfUrl;
+        link.download = "document.pdf"; // specify the filename
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
     return (
         <header className=" xl:px-[60px] bg-white banner-bg  dark:bg-gray-900 xl:pb-0">
             <div class="px-10 py-0" >
@@ -17,8 +26,8 @@ const Banner = () => {
                                     Hire Me
                                     <svg class="flex-shrink-0 w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6" /></svg>
                                 </a>
-                                <a class="py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#">
-                                    Contact sales team
+                                <a onClick={onButtonClick} class="py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#">
+                                    Downlode
                                 </a>
                             </div>
                         </div>
--- src/components/OurRoodMap/OurRoodMap.jsx
+++ src/components/OurRoodMap/OurRoodMap.jsx
@@ -72,7 +72,7 @@ const OurRoodMap = () => {
                             <p class="mt-2 leading-6">▶ Node.js,Express.js, Version Control   </p>
                             <p class="mt-2 leading-6">▶ Testing, Build Tools, RESTful APIs   </p>
                             <p class="mt-2 leading-6">▶ Debugging and DevTools, NPM  </p>
-                            <span class="absolute text-sm text-indigo-100/75 -top-5 left-2 whitespace-nowrap">Total 20 days</span>
+                            <span class="absolute text-sm text-indigo-100/75 -top-5 left-2 whitespace-nowrap">Total 60 days</span>
                         </div>
                         <div class="relative col-start-5 col-end-6 mr-7 md:mx-auto ">
                             <div class="flex items-center justify-center w-6 h-full">
@@ -99,11 +99,177 @@ const OurRoodMap = () => {
                             <p class="mt-2 leading-6">▶ Error Handling, React Route</p>
                             <p class="mt-2 leading-6">▶ Code Splitting, JSX syntax </p>
                             <p class="mt-2 leading-6">▶ Collaboration with Backend, Git </p>
+                            <span class="absolute text-sm text-indigo-100/75 -top-5 left-2 whitespace-nowrap">Total 50 days</span>
+                        </div>
+                    </div>
+
+
+
+
+                    <div class="flex md:contents flex-row-reverse">
+                        <div
+                            class="relative w-[350px] p-4 my-6 text-gray-800 bg-white rounded-xl col-start-1 col-end-5 mr-auto md:mr-0 md:ml-auto">
+                            <h3 class="text-lg font-semibold lg:text-xl">Firebase</h3>
+                            <p class="mt-2 leading-6">▶ Authentication</p>
+                            <p class="mt-2 leading-6">▶ Realtime Database: </p>
+                            <p class="mt-2 leading-6">▶ Firebase Hosting </p>
+                            <p class="mt-2 leading-6">▶ Firestore </p>
+                            <span class="absolute text-sm text-indigo-100/75 -top-5 left-2 whitespace-nowrap">Total 40 days</span>
+                        </div>
+                        <div class="relative col-start-5 col-end-6 mr-7 md:mx-auto ">
+                            <div class="flex items-center justify-center w-6 h-full">
+                                <div class="w-1 h-full bg-indigo-300 rounded-t-full bg-gradient-to-b from-indigo-400 to-indigo-300">
+                                </div>
+                            </div>
+                            <div class="absolute w-6 h-6 -mt-3 bg-white border-4 border-indigo-400 rounded-full top-1/2"></div>
+                        </div>
+                    </div>
+
+                    <div class="flex md:contents">
+                        <div class="relative col-start-5 col-end-6 mr-7 md:mx-auto">
+                            <div class="flex items-center justify-center w-6 h-full">
+                                <div class="w-1 h-full bg-indigo-300"></div>
+                            </div>
+                            <div class="absolute w-6 h-6 -mt-3 bg-white border-4 border-indigo-400 rounded-full top-1/2"></div>
+                        </div>
+                        <div class="relative p-4 my-6 text-gray-800 bg-white rounded-xl col-start-6 col-end-10 mr-auto w-[350px] ">
+                            <h3 class="text-lg font-semibold lg:text-xl">Bootstrap</h3>
+                            <p class="mt-2 leading-6">▶ Responsive Design </p>
+                            <p class="mt-2 leading-6">▶ Bootstrap Components </p>
+                            <p class="mt-2 leading-6">▶ Customization </p>
+                            <p class="mt-2 leading-6">▶ Grid System Mastery</p>
+                            <p class="mt-2 leading-6">▶ Typography and Icons</p>
+                            <p class="mt-2 leading-6">▶ Responsive Navigation </p>
+                            <p class="mt-2 leading-6">▶ Forms and Form Validation </p>
                             <span class="absolute text-sm text-indigo-100/75 -top-5 left-2 whitespace-nowrap">Total 20 days</span>
                         </div>
                     </div>
 
 
+
+
+                    <div class="flex md:contents flex-row-reverse">
+                        <div
+                            class="relative w-[350px] p-4 my-6 text-gray-800 bg-white rounded-xl col-start-1 col-end-5 mr-auto md:mr-0 md:ml-auto">
+                            <h3 class="text-lg font-semibold lg:text-xl">Tailwind CSS</h3>
+                            <p class="mt-2 leading-6">▶ Utility Classes</p>
+                            <p class="mt-2 leading-6">▶ Themability </p>
+                            <p class="mt-2 leading-6">▶ Plugins </p>
+                            <p class="mt-2 leading-6">▶ Dark Mode </p>
+                            <p class="mt-2 leading-6">▶ Responsive Design </p>
+                            <p class="mt-2 leading-6">▶ Bootstrap Components </p>
+                            <p class="mt-2 leading-6">▶ Customization </p>
+                            <p class="mt-2 leading-6">▶ Grid System Mastery</p>
+                            <p class="mt-2 leading-6">▶ Typography and Icons</p>
+                            <p class="mt-2 leading-6">▶ Responsive Navigation </p>
+                            <p class="mt-2 leading-6">▶ Forms and Form Validation </p>
+                            <span class="absolute text-sm text-indigo-100/75 -top-5 left-2 whitespace-nowrap">Total 30 days</span>
+                        </div>
+                        <div class="relative col-start-5 col-end-6 mr-7 md:mx-auto ">
+                            <div class="flex items-center justify-center w-6 h-full">
+                                <div class="w-1 h-full bg-indigo-300 rounded-t-full bg-gradient-to-b from-indigo-400 to-indigo-300">
+                                </div>
+                            </div>
+                            <div class="absolute w-6 h-6 -mt-3 bg-white border-4 border-indigo-400 rounded-full top-1/2"></div>
+                        </div>
+                    </div>
+
+                    <div class="flex md:contents">
+                        <div class="relative col-start-5 col-end-6 mr-7 md:mx-auto">
+                            <div class="flex items-center justify-center w-6 h-full">
+                                <div class="w-1 h-full bg-indigo-300"></div>
+                            </div>
+                            <div class="absolute w-6 h-6 -mt-3 bg-white border-4 border-indigo-400 rounded-full top-1/2"></div>
+                        </div>
+                        <div class="relative p-4 my-6 text-gray-800 bg-white rounded-xl col-start-6 col-end-10 mr-auto w-[350px] ">
+                            <h3 class="text-lg font-semibold lg:text-xl">Node.js</h3>
+                            <p class="mt-2 leading-6">▶ Installation and Setup </p>
+                            <p class="mt-2 leading-6">▶ Modules and npm </p>
+                            <p class="mt-2 leading-6">▶ CommonJS Modules </p>
+                            <p class="mt-2 leading-6">▶ Event Loop</p>
+                            <p class="mt-2 leading-6">▶ HTTP Module</p>
+                            <p class="mt-2 leading-6">▶ npm Scripts </p>
+                            <p class="mt-2 leading-6">▶ Debugging </p>
+                            <span class="absolute text-sm text-indigo-100/75 -top-5 left-2 whitespace-nowrap">Total 20 days</span>
+                        </div>
+                    </div>
+
+
+
+                    <div class="flex md:contents flex-row-reverse">
+                        <div
+                            class="relative w-[350px] p-4 my-6 text-gray-800 bg-white rounded-xl col-start-1 col-end-5 mr-auto md:mr-0 md:ml-auto">
+                            <h3 class="text-lg font-semibold lg:text-xl">Express.js</h3>
+                            <p class="mt-2 leading-6">▶ Routing</p>
+                            <p class="mt-2 leading-6">▶ Middleware </p>
+                            <p class="mt-2 leading-6">▶ Request and Response Handling </p>
+                            <p class="mt-2 leading-6">▶ Error Handling </p>
+                            <p class="mt-2 leading-6">▶ Form Handling </p>
+                            <p class="mt-2 leading-6">▶ Authentication and Authorization </p>
+                            <p class="mt-2 leading-6">▶ API Development </p>
+                            <span class="absolute text-sm text-indigo-100/75 -top-5 left-2 whitespace-nowrap">Total 30 days</span>
+                        </div>
+                        <div class="relative col-start-5 col-end-6 mr-7 md:mx-auto ">
+                            <div class="flex items-center justify-center w-6 h-full">
+                                <div class="w-1 h-full bg-indigo-300 rounded-t-full bg-gradient-to-b from-indigo-400 to-indigo-300">
+                                </div>
+                            </div>
+                            <div class="absolute w-6 h-6 -mt-3 bg-white border-4 border-indigo-400 rounded-full top-1/2"></div>
+                        </div>
+                    </div>
+
+                    <div class="flex md:contents">
+                        <div class="relative col-start-5 col-end-6 mr-7 md:mx-auto">
+                            <div class="flex items-center justify-center w-6 h-full">
+                                <div class="w-1 h-full bg-indigo-300"></div>
+                            </div>
+                            <div class="absolute w-6 h-6 -mt-3 bg-white border-4 border-indigo-400 rounded-full top-1/2"></div>
+                        </div>
+                        <div class="relative p-4 my-6 text-gray-800 bg-white rounded-xl col-start-6 col-end-10 mr-auto w-[350px] ">
+                            <h3 class="text-lg font-semibold lg:text-xl">MongoDB</h3>
+                            <p class="mt-2 leading-6">▶ Understanding JSON </p>
+                            <p class="mt-2 leading-6">▶ Document Structure </p>
+                            <p class="mt-2 leading-6">▶ MongoDB Query </p>
+                            <p class="mt-2 leading-6">▶ CRUD Operations</p>
+                            <p class="mt-2 leading-6">▶ Basic Data Modeling</p>
+                            <p class="mt-2 leading-6">▶ Error Handling </p>
+                            <p class="mt-2 leading-6">▶ MongoDB Atlas </p>
+                            <span class="absolute text-sm text-indigo-100/75 -top-5 left-2 whitespace-nowrap">Total 40 days</span>
+                        </div>
+                    </div>
+
+
+                    <div class="flex md:contents flex-row-reverse">
+                        <div
+                            class="relative w-[350px] p-4 my-6 text-gray-800 bg-white rounded-xl col-start-1 col-end-5 mr-auto md:mr-0 md:ml-auto">
+                            <h3 class="text-lg font-semibold lg:text-xl">For state management</h3>
+                            <p class="mt-2 leading-6">▶ TanStack Query</p>
+                            <p class="mt-2 leading-6">▶ Context API  </p>
+                            <span class="absolute text-sm text-indigo-100/75 -top-5 left-2 whitespace-nowrap">Total 20 days</span>
+                        </div>
+                        <div class="relative col-start-5 col-end-6 mr-7 md:mx-auto ">
+                            <div class="flex items-center justify-center w-6 h-full">
+                                <div class="w-1 h-full bg-indigo-300 rounded-t-full bg-gradient-to-b from-indigo-400 to-indigo-300">
+                                </div>
+                            </div>
+                            <div class="absolute w-6 h-6 -mt-3 bg-white border-4 border-indigo-400 rounded-full top-1/2"></div>
+                        </div>
+                    </div>
+
+                    <div class="flex md:contents">
+                        <div class="relative col-start-5 col-end-6 mr-7 md:mx-auto">
+                            <div class="flex items-center justify-center w-6 h-full">
+                                <div class="w-1 h-full bg-indigo-300"></div>
+                            </div>
+                            <div class="absolute w-6 h-6 -mt-3 bg-white border-4 border-indigo-400 rounded-full top-1/2"></div>
+                        </div>
+                        <div class="relative p-4 my-6 text-gray-800 bg-white rounded-xl col-start-6 col-end-10 mr-auto w-[350px] ">
+                            <h3 class="text-lg font-semibold lg:text-xl">For Authentication</h3>
+                            <p class="mt-2 leading-6">▶ Firebase </p>
+                            <p class="mt-2 leading-6">▶ JWT</p>
+                            <span class="absolute text-sm text-indigo-100/75 -top-5 left-2 whitespace-nowrap">Total 30 days</span>
+                        </div>
+                    </div>
 
                 </div>
             </Parallax>
